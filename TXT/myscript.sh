f5adb87e76227a19d082519e1fefc9e0d83bcdba
chandapap <60642621+chandapap@users.noreply.github.com>
2020-10-12 09:33:10 +0700

    Create myscript.sh

---
+++ TXT/myscript.sh
@@ -0,0 +1,15 @@
+ls => list directory content => ls -a
+cd => change directory => cd/example
+cp => copy file => cp source destination
+rm => remove file => rm file
+mv => move file => mv filefrom fileto
+mkdir => make directory => mkdir directoryname
+rmdir => remove directory => rmdir directoryname
+cat => read file => cat filename
+grep => search string in file => grep word file
+top => displat system task => top
+chmod => change access => chmod 777
+chown => change owner => chown user filename
+sudo => use root permission => sudo other command
+tar => tar command => tar cf (archive to tar), tar tf (list .tar file), tar xf (extract tar file)
+date => see date and time on system
